fa0778b90a8ea32f9beb2be44edaa2e0dce71175
Joan Shamet <shametijoan@gmail.com>
2023-07-17 14:08:26 +0200

    finishing routes

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -4,4 +4,6 @@ const ordersController = require("../controllers/ordersController");
 module.exports = function (app) {
     app.post("/coffeshop/orders",ordersController.createOrder);
     app.get("/coffeshop/orders",ordersController.getAllOrders);
+    app.get("./coffeshop/orders/:id",ordersController.getOrderById);
+    app.put("./coffeshop/orders/:id",ordersController.updateOrder);
 }
